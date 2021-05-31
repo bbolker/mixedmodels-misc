82f549b2f5de6299acfd04a8047c99852da93a4d
magronah <lampofgod7@gmail.com>
2021-05-30 22:49:35 -0400

    Further changes and comments fixed

--- taskview/GLMM_RUN.R
+++ taskview/GLMM_RUN.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/Michae/OneDrive/Documents/McMaster/R/GLMM_Cran_Task/mixedmodels-misc/taskview")
+setwd("~/R/project/mixedmodels-misc/taskview")
 #install.packages("ctv")
 #library(XML)
 #vignette("ctv-howto")
@@ -8,32 +8,4 @@ library("ctv")
 ctv2html("MixedModels.ctv")
 browseURL("MixedModels.html")
 
-#<h4>Model diagnostics and summary statistics</h4>
-#  <pkg>influence.ME</pkg>, <pkg>aods3</pkg>, <pkg>cAIC4</pkg>, <pkg>HLMdiag</pkg>, , 
-
-#<pkg>lmmfit</pkg>, <pkg>iccbeta</pkg>, 
-
-#HLMdiag </pkg> contains diagnostic tools for linear models mixed models.  
-#<pkg> asremlPlus </pkg>
-
-#The following packages contain data sets, sampling methods for 
-#mixed model analysis:  <pkg>broom.mixed:</pkg>; 
-#<pkg> blmeco </pkg>; <pkg> nlmeU </pkg>; <pkg> lmeresampler </pkg>; 
-#<pkg> StroupGLMM </pkg>; <pkg> SASmixed</pkg>; <pkg> glmmboot </pkg>. 
-#<pkg> StroupGLMM </pkg> also contains some R codes for generalized linear mixed models
-
-# <ul>  <li> ... </li> <li> .... </li> </ul>
-
-# <pkg> biglmm </pkg>: Regression for  Linear and Generalized Linear Models data too large to fit in memory;
-# <pkg> sparseFLMM</pkg>: Estimates parameters for Functional Linear Mixed
-#Models for Irregularly or Sparsely Sampled Data; 
-#<pkg> denseFLMM </pkg>: Estimates parameter of a 
-#functional Linear Mixed Models for Densely Sampled Data; 
-
-#<pkg>broom.mixed:</pkg>; <pkg> blmeco </pkg>; 
-#<pkg> nlmeU </pkg>; <pkg> lmeresampler </pkg> 
-#Bootstrap routines for nested linear mixed effects models fit 
-#using either 'lme4' or 'nlme'; <pkg> StroupGLMM </pkg>
-#: R Codes and Datasets for Generalized Linear Mixed Models; 
-#<pkg> SASmixed</pkg>: Data sets from "SAS System for Mixed Models";
-#<pkg> glmmboot </pkg>: Performs Bootstrap Resampling for Mixed Effects and Plain Models
+ 
